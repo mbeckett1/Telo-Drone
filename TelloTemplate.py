bd91b6b9247e0bd2332cf2885e5dcc45784ef277
Mark Beckett <02mark.beckett@gulllakecs.org>
2020-11-10 10:45:18 -0800

    Converted Square to a Function

--- TelloTemplate.py
+++ TelloTemplate.py
@@ -43,6 +43,13 @@ recvThread.start()
 
 # CREATE FUNCTIONS HERE....
 
+# Make a Square Function
+def square():
+    sendmsg('up 50')
+    for i in range(4)
+        sendmsg('forward 100')
+        sendmsg('ccw 90')
+
 
 print("\nMark Beckett")
 print("Program Name: Tello Drone Training School")
@@ -60,11 +67,8 @@ try:
         sendmsg('command', 0)
         sendmsg('takeoff')
 
-        # Make a square
-        sendmsg('up 50')
-        for i in range(4)
-            sendmsg('forward 100')
-            sendmsg('ccw 90')
+        #Call the Square Function
+        square()
 
         sendmsg('land')
 
